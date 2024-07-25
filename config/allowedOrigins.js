d31b1806164a9e4a5d8e4eed27eed1543aafddc3
pawan singh <120004900+pawan1222@users.noreply.github.com>
2024-07-25 11:54:49 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,9 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-'https://blogging-backend-c37u.onrender.com'
+'https://blogging-backend-c37u.onrender.com',
+'https://pawan-blogging-app.netlify.app'
+
 
 
 ];
